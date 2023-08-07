a6f7d7d26529ac5298f1e06e27bdab190ed16e51
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-08-08 00:17:19 +0700

    config env

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
 import express from 'express'
 import configViewEngine from './configs/viewEngine'
 const path = require('path')
+require('dotenv').config()
+
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8888
+const hostname = process.env.HOST_NAME
 
 configViewEngine(app)
 app.get('/', (req, res) => {
     res.render('index.ejs')
 })
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
