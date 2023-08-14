88853115210978e46f0774f2be19a9f19cff2b26
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-08-14 23:59:25 +0700

    Design Form Add New User

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,13 +1,7 @@
 const connection = require('../config/database')
 
 const getHomepage = (req, res) => {
-    connection.query(
-        'SELECT * FROM Users u ',
-        function (err, results, fields) {
-            console.log('>>> result: ', results);
-            res.render('index.ejs')
-        }
-    );
+    return res.render('home.ejs')
 }
 
 module.exports = {
