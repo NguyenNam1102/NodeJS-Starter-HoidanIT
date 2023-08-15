9d6eeec8cf59c2cb27518220fec233436764ca71
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-08-15 23:59:28 +0700

    create user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,6 +4,22 @@ const getHomepage = (req, res) => {
     return res.render('home.ejs')
 }
 
+const postCreateUser = (req, res) => {
+    console.log(req.body);
+    let { email, name, city } = req.body
+
+    connection.query(
+        `INSERT INTO Users (email, name, city) 
+            VALUES(?, ?, ?) `,
+        [email, name, city],
+        function (err, results) {
+            console.log(results);
+            res.send('Created user succeed!')
+        }
+    );
+}
+
 module.exports = {
-    getHomepage
+    getHomepage,
+    postCreateUser
 }
--- src/routers/web.js
+++ src/routers/web.js
@@ -1,8 +1,9 @@
 const express = require('express')
 
 const router = express.Router()
-const { getHomepage } = require('../controllers/homeController')
+const { getHomepage, postCreateUser } = require('../controllers/homeController')
 
 router.get('/', getHomepage)
+router.post('/create-user', postCreateUser)
 
 module.exports = router;
